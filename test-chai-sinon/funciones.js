0c5deecbc110d0349e1360a1ce5e434674f7209f
PRATECH\amontoya <amontoya@pratechgroup.com>
2021-05-28 05:55:11 -0500

    Ejemplo chai-spies

--- test-chai-sinon/funciones.js
+++ test-chai-sinon/funciones.js
@@ -7,18 +7,20 @@
 };
 
 const suma = (valor1, valor2) => {
-  console.log("SUMANDO");
+  console.log("Se realizará suma");
   return valor1 + valor2;
 };
 const resta = (valor1, valor2) => valor1 - valor2;
 
-module.exports = { ejecutarOperacion, suma }; */
-
+module.exports = { ejecutarOperacion, suma };
+ */
 module.exports = {
   ejecutarOperacion: function (operacion, valor1, valor2) {
     if (operacion == "S") {
       this.suma(valor1, valor2);
+      console.log("Se realizará suma");
     } else {
+      console.log("Se realizará resta");
       this.resta(valor1, valor2);
     }
   },
--- test-chai-sinon/test/example-should.test.js
+++ test-chai-sinon/test/example-should.test.js
@@ -9,9 +9,14 @@ chai.use(sinonChai);
 describe("Usar calculadora", function () {
   it("puede llamar la funcion de Suma", () => {
     const resultado = 5;
-    const suma = sinon.spy(r, "suma");
+    const sumaSpy = sinon.spy(r, "suma");
+    const consoleSpy = sinon.spy(console, "log");
     r.ejecutarOperacion("S", 2, 3);
-    //suma.should.have.callCount(0);
-    sinon.assert.callCount(suma, 1);
+
+    sumaSpy.should.have.callCount(1);
+    //sinon.assert.callCount(sumaSpy, 1);
+    //sumaSpy.should.have.been.calledWith(2, 4);
+    //sumaSpy.should.have.returned(5);
+    //sumaSpy.should.have.been.calledAfter(consoleSpy);
   });
 });
